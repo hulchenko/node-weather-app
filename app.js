3542ec905ce739a8b0f2b05c3fb2e40fda595a50
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-07-02 15:48:59 -0600

    reset the app

--- app.js
+++
@@ -1,8 +0,0 @@
-const request = require('request');
-const url =
-  'http://api.weatherstack.com/current?access_key=95b925454ccd351b9f51eb37f161b42a&query=37.8267,-122.4233';
-
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
-});
